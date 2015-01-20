8aed2d5b84dde228a559f816f944cdb89cd079fb
Andrea Cecchi <andrea.cecchi85@gmail.com>
2015-01-20 16:06:51 +0100

    created repo

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='pas.plugins.proxy',
+      version=version,
+      description="A pas plugin for roles proxy between users",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='pas plugin proxy',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='https://github.com/RedTurtle/pas.plugins.proxy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['pas', 'pas.plugins'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
