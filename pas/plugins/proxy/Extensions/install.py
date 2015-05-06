ae14bed9112c2c503e73416f10e1f630b4af06d2
Luca Fabbri <luca@keul.it>
2015-05-06 15:26:55 +0200

    removed install function, not required

--- pas/plugins/proxy/Extensions/install.py
+++ pas/plugins/proxy/Extensions/install.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-def install(portal):
-    setup_tool = portal.portal_setup
-    setup_tool.runAllImportStepsFromProfile('profile-pas.plugins.proxy:default')
-
-
 def uninstall(portal, reinstall=False):
     if not reinstall:
         setup_tool = portal.portal_setup
